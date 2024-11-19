3ef0ceb06a0db284b5ada5172123c30d753ee700
chiakimorisawafanpage <polika1213sdf@gmail.com>
2024-11-20 01:27:40 +0300

    Добавьте файлы проекта.

---
+++ dz 20.11.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+double power(double number, int exponent) {
+    double result = 1.0;
+
+    if (exponent > 0) {
+        for (int i = 0; i < exponent; ++i) {
+            result *= number;
+        }
+    }
+    else if (exponent < 0) {
+        for (int i = 0; i < -exponent; ++i) {
+            result *= number;
+        }
+        result = 1.0 / result;
+    }
+    return result;
+}
+
+int main() {
+    double number;
+    int exponent;
+
+    cout << "число: ";
+    cin >> number;
+
+    cout << "степень: ";
+    cin >> exponent;
+
+    double result = power(number, exponent);
+    cout << number <<" " << exponent <<" " << result << endl;
+
+    return 0;
+}
